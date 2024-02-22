e579287877e131ed89d58213cf4f6ada49b10a68
unknown <boris.galac@gmail.com>
2024-02-22 15:06:45 +0100

    aa

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,10 @@
 var swiper = new Swiper(".mySwiper", {
+  loop: true,
+  speed: 1000,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
   effect: "cards",
   grabCursor: true,
 });
