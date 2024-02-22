b0293ce552ffc1193920de72c22ca9e3abf6795a
unknown <boris.galac@gmail.com>
2024-02-22 14:39:03 +0100

    aa

--- src/js/main.js
+++ src/js/main.js
@@ -1,35 +1,4 @@
-// const swiper = new Swiper(".swiper", {
-//   // Optional parameters
-//   direction: "horizontal",
-//   loop: true,
-
-//   // If we need pagination
-//   pagination: {
-//     el: ".swiper-pagination",
-//   },
-
-//   // Navigation arrows
-//   navigation: {
-//     nextEl: ".swiper-button-next",
-//     prevEl: ".swiper-button-prev",
-//   },
-
-//   // And if we need scrollbar
-//   scrollbar: {
-//     el: ".swiper-scrollbar",
-//   },
-// });
-
 var swiper = new Swiper(".mySwiper", {
-  effect: "cube",
+  effect: "cards",
   grabCursor: true,
-  cubeEffect: {
-    shadow: true,
-    slideShadows: true,
-    shadowOffset: 20,
-    shadowScale: 0.94,
-  },
-  pagination: {
-    el: ".swiper-pagination",
-  },
 });
